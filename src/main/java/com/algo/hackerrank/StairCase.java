e8550e39091534e7bfcf9ca39ef52773891f909a
mddanish-ali <danishsym@gmail.como>
2026-07-11 18:34:59 +0530

    Stair case program.

---
+++ src/main/java/com/algo/hackerrank/StairCase.java
@@ -0,0 +1,22 @@
+package com.algo.hackerrank;
+
+public class StairCase {
+
+    public static void main(String[] args) {
+        staircase(6);
+    }
+
+    public static void staircase(int n) {
+        // Write your code here
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+    }
+}
