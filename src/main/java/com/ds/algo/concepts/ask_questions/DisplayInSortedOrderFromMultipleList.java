83003ba2e82f130a954bfb0f1295c8183a731a6e
mddanish-ali <danishsym@gmail.como>
2026-07-23 12:27:47 +0530

    Fix compile time error

--- src/main/java/com/ds/algo/concepts/ask_questions/DisplayInSortedOrderFromMultipleList.java
+++ src/main/java/com/ds/algo/concepts/ask_questions/DisplayInSortedOrderFromMultipleList.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class DisplayInSortedOrderFromMultipleList {
-    static void main() {
+    public static void main(String[] args) {
         List<Integer> list1 = Arrays.asList(1, 4, 7, 10);
         List<Integer> list2 = Arrays.asList(2, 5, 8, 11);
         List<Integer> list3 = Arrays.asList(3, 6, 9, 12);
